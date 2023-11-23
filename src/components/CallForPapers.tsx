b627327f485097ca28de357310bbbb5d27fb9237
alanansari <ansarialan31@gmail.com>
2023-11-23 12:05:06 +0530

    ADD: About Section

---
+++ src/components/CallForPapers.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CallForPapers() {
+  return (
+    <div>
+      <h2>Call For Papers</h2>
+      
+
+    </div>
+  )
+}
+
+export default CallForPapers
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,8 +11,8 @@ const Hero = () => {
               <div className='text-[2rem] lg:text-[2.5rem] font-bold tracking-widest py-10 leding-[2rem] lg:leading-[3.5rem] w-[85%]'>
                 INTERNATIONAL CONFERENCE ON LARGE LANGUAGE MODELS
               </div>
-              <div className='text-xl w-[70%]'>
-                Organised by IT Department ,AKGEC Ghaziabad
+              <div className='text-lg w-[70%]'>
+                Organised by Department of Information Technology, Ajay Kumar Garg Engineering College, Ghaziabad, Uttar Pradesh, India
               </div>
               <div className='text-lg w-[70%]'>Jan 17-23,2024</div>
               <div className='pt-8'>
@@ -27,17 +27,17 @@ const Hero = () => {
             width={730}
             height={730}
          />
-         <div className='mt-[20vh] xs:mt-[15vh] md:hidden text-white' style={{
+         <div className='mt-[20vh] xs:mt-[15vh] h-[80vh] xs:h-auto md:hidden text-white' style={{
           backgroundImage: 'url("conf.png")'
          }}>
             <div className='w-[80%] ml-5'>
               <div className='text-[1.5rem] font-bold tracking-widest py-10 leding-[2rem] w-[90%]'>
                 INTERNATIONAL CONFERENCE ON LARGE LANGUAGE MODELS
               </div>
-              <div className='text-lg font-bold'>
-                Organised by IT Department, AKGEC Ghaziabad
+              <div className='text-base font-bold'>
+                Organised by Department of Information Technology, Ajay Kumar Garg Engineering College, Ghaziabad, Uttar Pradesh, India
               </div>
-              <div className='text-lg w-[70%]'>Jan 17-23,2024</div>
+              <div className='text-lg w-[70%]'>24 & 25 May 2024</div>
               <div className='pt-8'>
                 <Link href='' className='text-lg px-12 py-4 bg-[#1983ff]'>Register</Link>
               </div>
